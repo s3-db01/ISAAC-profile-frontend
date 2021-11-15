209c5114c6c65cb88f3c763a36f5c67393aaf128
Eykholt,Tom T.T.H <t.eykholt@student.fontys.nl>
2021-11-15 14:13:02 +0100

    POC fully functional

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -7,27 +7,57 @@ class UserInfo extends Component {
         this.state = {
             name: "",
             email: "",
-            id: ""
+            id: "",
+            preftemp: ""
         };
+        this.getUserPrefTemp()
         this.props.keycloak.loadUserInfo().then(userInfo => {
-            this.setState({name: userInfo.name, email: userInfo.email, id: userInfo.sub})
+            this.setState({name: userInfo.name, email: userInfo.email, id: userInfo.sub, preftemp: localStorage.getItem('preftemp')})
+            localStorage.setItem("userId", userInfo.sub)
         });
     }
 
-    handleSubmit(event){
-        fetch('restapi/'+this.state.id+'/'+ event.target.preftemp.value, {
+    handleSubmit(e) {
+        fetch('http://localhost:3001/' + localStorage.getItem('userId') + '/favorites/temp/' + this.state.preftemp, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             }
         }).then(response => {
             if (response.status >= 200 && response.status < 300) {
-                console.log(response);
-                return response;
+                localStorage.setItem("preftemp", response.data.preftemp);
             } else {
                 console.log('Somthing happened wrong');
             }
         }).catch(err => err);
+
+        this.getUserPrefTemp()
+    }
+
+
+    getUserPrefTemp(){
+        fetch('http://localhost:3001/'+localStorage.getItem('userId') +'/favorites/temp', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                response.json().then(
+                    data =>
+                        localStorage.setItem("preftemp", data.data[0].preftemp )
+                )
+                console.log(localStorage.getItem('preftemp'));
+            } else {
+                console.log('Somthing happened wrong');
+            }
+        }).catch(err => err);
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
@@ -36,11 +66,11 @@ class UserInfo extends Component {
                 <p>ID: {this.state.id}</p>
                 <p>Welcome {this.state.name}</p>
                 <p>Settings:</p>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit.bind(this)}>
                     <label htmlFor="email">Email</label><br/>
                     <input type="text" id="email" name="email" value={this.state.email}/><br/>
                     <label htmlFor="preftemp">Preferd Temperature</label><br/>
-                    <input type="text" id="preftemp" name="preftemp" /><br/>
+                    <input type="text" id="preftemp" name="preftemp" value={this.state.preftemp} onChange={this.handleInputChange.bind(this)}/><br/>
                     <input type="submit" value="Submit"/>
                 </form>
             </div>
